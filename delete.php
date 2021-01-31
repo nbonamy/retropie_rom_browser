0865cb7487e0aed18d007e123817023242192b0a
Nicolas Bonamy <nicolas@bonamy.fr>
2021-01-31 11:50:10 -0600

    delete/achive test

--- delete.php
+++ delete.php
@@ -5,9 +5,35 @@
 require_once('romsite.php');
 require_once('gamelist.php');
 
-// delete rom
-$filename = BASE_ROM."$system/".$_GET['filename'];
-if (unlink($filename) === TRUE) {
+// rom filename
+$romdir = BASE_ROM."$system/";
+$filename = $romdir.$_GET['filename'];
+
+// archive or delete
+$rc = FALSE;
+if (FALSE){//$config['archive'] == TRUE) {
+
+  // archive dir
+  $archive_dir = "$romdir/archives/";
+  if (file_exists($archive_dir) === FALSE) {
+    $rc = mkdir($archive_dir);
+  } else {
+    $rc = TRUE;
+  }
+
+  // move file
+  if ($rc === TRUE) {
+    $rc = rename($filename, $archive_dir.$_GET['filename']);
+  }
+
+} else {
+
+  // delete
+  $rc = unlink($filename);
+
+}
+
+if ($rc === TRUE) {
 
   // delete image
   $image = BASE_IMAGE."$system/".$_GET['image'];
--- romsite.php
+++ romsite.php
@@ -1,14 +1,24 @@
 <?php
 
 // paths
-define("BASE_ROM", "/home/pi/RetroPie/roms/");
-define("BASE_IMAGE", "/home/pi/.emulationstation/downloaded_images/");
-define("BASE_GAMELIST", "/home/pi/.emulationstation/gamelists/");
-define("IGNORED_FILENAMES", array("tos.img"));
-define("IGNORED_EXTS", array("srm"));
+define('BASE_ROM', '/home/pi/RetroPie/roms/');
+define('BASE_IMAGE', '/home/pi/.emulationstation/downloaded_images/');
+define('BASE_GAMELIST', '/home/pi/.emulationstation/gamelists/');
+define('IGNORED_FILENAMES', array('tos.img'));
+define('IGNORED_EXTS', array('srm', 'bin', 'jpg', 'conf'));
 
 // global
-$system = $_GET['system'];
+$system = @$_GET['system'];
+
+// load config
+$config_filename = __DIR__.'/config.ini';
+if (file_exists($config_filename)) {
+  $config = parse_ini_file($config_filename);
+} else {
+  $config = array();
+}
+
+// some helper functions
 
 function contains($haystack, $needle) {
   return stripos($haystack, $needle) !== FALSE;
