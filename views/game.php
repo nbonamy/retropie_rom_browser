9626515519c59494fc75f6aa3d629f1efe79ea96
Nicolas Bonamy <nicolas@bonamy.fr>
2021-02-02 22:04:54 -0600

    enhanced cover title

--- views/game.php
+++ views/game.php
@@ -2,7 +2,7 @@
 <div class="game" data-system="<?= $system ?>" data-name="<?= $filename ?>" data-image="<?= $cover ?>">
   <div class="action right delete">×</div>
   <div class="action left favorite <?= $favorite ? 'active' : '' ?>"><?= $favorite ? '♥️' : '♡' ?></div>
-  <img src="<?= $image ?>" title="<?= $filename ?>">
+  <img src="<?= $image ?>" title="<?= $filename ?> - <?= $title ?>">
   <div class="content">
     <span><?= $title ?></span>
   </div>
